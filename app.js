838f497af54115ac3b8420833b42f984c2ed68d4
Mike <mclancy96@gmail.com>
2022-03-26 09:41:00 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express"),
+    app = express(),
+    dpci_handler = require("./controllers/dpci");
+
+require('dotenv').config();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req,res) => {
+	res.render("./index");
+});
+
+//Temporary route without API key
+app.post("/", (req,res)=>{
+    res.render("./item")
+})
+
+
+//Correct way to do it
+//app.post("/", (req,res)=>{dpci_handler.handleDPCI(req,res);})
+
+app.listen(process.env.PORT, function(){
+	console.log("App is running on " + process.env.PORT);
+});
